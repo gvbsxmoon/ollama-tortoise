6b51982b99067ab57f74178cb49c6285c2a894db
gvbsxmoon <57920689+gvbsxmoon@users.noreply.github.com>
2024-11-26 08:41:10 +0100

    Update tortoise.py

--- app/plugin/tortoise.py
+++ app/plugin/tortoise.py
@@ -1,6 +1,12 @@
-
+# from torchaudio import save
 from tortoise.api import TextToSpeech
 from tortoise.utils.audio import load_voice
+
+# if you want to test standalone tortoise
+# you can do something like this
+
+# audio = generate_audio("hello friends, i'm simona", "emma")
+# save("test.wav", audio.squeeze(0).cpu(), 24000)
 
 def generate_audio(text, voice):
     tts = TextToSpeech()
